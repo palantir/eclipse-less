0a70ef842d2a9474eb2e3ccaefb8b3886a508c5e
Derek Cicerone <dcicerone@palantir.com>
2014-01-25 18:20:20 -0500

    add copyright/license notice

--- com.palantir.less/bin/main.js
+++ com.palantir.less/bin/main.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var fs = require("fs");
 var less = require("less");
 
@@ -5,11 +21,11 @@ var less = require("less");
 var lessFile = process.argv[process.argv.length - 2];
 var lessContents = fs.readFileSync(lessFile, "utf-8");
 
-// compile
 var parser = new less.Parser({
 	filename: lessFile
 });
 
+// compile the LESS file to CSS
 parser.parse(lessContents, function (e, tree) {
 	var cssContents = tree.toCSS();
 	var cssFile = process.argv[process.argv.length - 1];
