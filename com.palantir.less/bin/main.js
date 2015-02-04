579edd96ab9358b672696194e678c3a46d03428d
Ashwin Ramaswamy <aramaswamy@palantir.com>
2015-02-04 16:45:58 -0500

    update less to 2.3.1

--- com.palantir.less/bin/main.js
+++ com.palantir.less/bin/main.js
@@ -21,13 +21,8 @@ var less = require("less");
 var lessFile = process.argv[process.argv.length - 2];
 var lessContents = fs.readFileSync(lessFile, "utf-8");
 
-var parser = new less.Parser({
-	filename: lessFile
-});
-
 // compile the LESS file to CSS
-parser.parse(lessContents, function (e, tree) {
-	var cssContents = tree.toCSS();
-	var cssFile = process.argv[process.argv.length - 1];
-	fs.writeFileSync(cssFile, cssContents, "utf-8");
+less.render(lessContents, { filename: lessFile }, function(error, output) {
+    var cssFile = process.argv[process.argv.length - 1];
+    fs.writeFileSync(cssFile, output.css, "utf-8");
 });
